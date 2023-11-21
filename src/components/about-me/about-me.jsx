dc674298278e03908667ac646383a0d0e5497bfb
Tobias <tobiasramossilva@gmail.com>
2023-11-21 14:33:44 -0300

    Fazendo o componente sobre mim

---
+++ src/components/about-me/about-me.jsx
@@ -0,0 +1,20 @@
+const AboutMe = () => {
+  return (
+    <div>
+      <h1>Sobre mim</h1>
+      <p>
+        Front end com mais de 2 anos de experiência em desenvolvimento de
+        aplicações web, fazendo o uso de frameworks e bibliotecas modernos.
+        Minha jornada é marcada por uma constante busca por evolução, participai
+        de projetos desafiadores, explorando tecnologias que eram novas para
+        mim, experiências como essas expandiu meus conhecimentos e habilidades.
+        Além desse meu lado profissional, sou também um grande praticante de
+        esportes. Tenho um imenso apego ao muay thai, e ao ciclismo, em ambos
+        esportes já participei de competições e assim é a minha rotina,
+        estuando, trabalhando e praticando esses esportes.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
