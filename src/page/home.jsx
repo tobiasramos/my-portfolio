6934316bf21276bba945b5e7e79f419307a72342
Tobias <tobiasramossilva@gmail.com>
2023-11-27 14:15:09 -0300

    Deixando o componente sobre mim, habilidades responsivos

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -7,55 +7,61 @@ import Contact from "../components/contact/contact";
 import styles from "./Home.module.css";
 import { Tab } from "@mui/material";
 import Projects from "../components/projects/projects";
+import Footer from "../components/footer/footer";
+import Header from "../components/header/header";
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      <Router>
-        <nav className={styles.link}>
-          <ul>
-            <Tab
-              className={styles.tab}
-              label="Sobre mim"
-              component={Link}
-              to="/"
-            />
-            <Tab
-              className={styles.tab}
-              label="Experiência"
-              component={Link}
-              to="/experience"
-            />
-            <Tab
-              className={styles.tab}
-              label="Habilidades"
-              component={Link}
-              to="/skills"
-            />
-            <Tab
-              className={styles.tab}
-              label="Projetos"
-              component={Link}
-              to="/projects"
-            />
-            <Tab
-              className={styles.tab}
-              label="Contato"
-              component={Link}
-              to="/contact"
-            />
-          </ul>
-        </nav>
-        <div className={styles.rendering}>
-          <Routes>
-            <Route path="/" element={<AboutMe />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-      </Router>
+        <Header />
+      <div className={styles.main}>
+        <Router>
+          <nav className={styles.link}>
+            <ul>
+              <Tab
+                className={styles.tab}
+                label="Sobre mim"
+                component={Link}
+                to="/"
+              />
+              <Tab
+                className={styles.tab}
+                label="Experiência"
+                component={Link}
+                to="/experience"
+              />
+              <Tab
+                className={styles.tab}
+                label="Habilidades"
+                component={Link}
+                to="/skills"
+              />
+              <Tab
+                className={styles.tab}
+                label="Projetos"
+                component={Link}
+                to="/projects"
+              />
+              <Tab
+                className={styles.tab}
+                label="Contato"
+                component={Link}
+                to="/contact"
+              />
+            </ul>
+          </nav>
+          <div className={styles.rendering}>
+            <Routes>
+              <Route path="/" element={<AboutMe />} />
+              <Route path="/experience" element={<Experience />} />
+              <Route path="/skills" element={<Skills />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+      <Footer />
     </div>
   );
 };
