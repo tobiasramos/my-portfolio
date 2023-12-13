893f79a8dd5fafe1a29e1d5ea13f3e638589742d
Tobias <tobiasramossilva@gmail.com>
2023-12-13 16:25:24 -0300

    Atualizando currículo e o texto sobre mim

--- src/components/about-me/about-me.jsx
+++ src/components/about-me/about-me.jsx
@@ -8,19 +8,22 @@ const AboutMe = () => {
       <h2>Sobre mim</h2>
       <div className={styles.container}>
         <p className={styles.summary}>
-          Front end com mais de 2 anos de experiência em desenvolvimento de
-          aplicações web, fazendo o uso de frameworks e bibliotecas modernos.
+          Front end com quase 2 anos de experiência em desenvolvimento de
+          aplicações web, fazendo o uso de bibliotecas e frameworks modernos.
         </p>
         <p className={styles.summary}>
           Minha jornada é marcada por uma constante busca por evolução,
-          participai de projetos desafiadores, explorando tecnologias que eram
-          novas para mim, experiências como essas expandiram meus conhecimentos
-          e habilidades.
+          frequentemente participo de projetos desafiadores, explorando novas
+          tecnologias, ajudando pessoas e enriquecendo meus conhecimentos e
+          habilidades. Um grande passo foi participar do desenvolvimento ao lado
+          server (back end), entendendo como funcionar toda estrutura e lógica por trás
+          de uma aplicação.
         </p>
         <p className={styles.summary}>
-          Além do meu lado profissional, sou também um grande praticante de
-          esportes. Tenho um imenso apego ao muay thai, e ao ciclismo, em ambos
-          esportes participo de competições.
+          Saindo um pouco do meu lado profissional, sou também um grande
+          praticante de esportes. Tenho um imenso apego ao muay thai, e ao
+          ciclismo, em ambos esportes participo de competições. Tenho abito de
+          ler, livros relacionados a tecnologias e também a literatura.
         </p>
       </div>
     </section>
